f8714357e32df6a4fce8b16ebd666287cd4b1095
Krish Prajapati <krishprajapati463@gmail.com>
2026-01-04 14:32:28 +0530

    Add starting files

--- chatbot-project/src/App.jsx
+++ chatbot-project/src/App.jsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { ChatInput } from './components/ChatInput';
 import { ChatMessages } from './components/ChatMessages';
 import './App.css'
 
 function App() {
   const [chatMessages, setChatMessages] = useState([]);
+
+  useEffect(()=>{
+    localStorage.setItem('messages',JSON.stringify())
+  },[chatMessages])
+
+
   return (
     <div className="app-container">
       {chatMessages.length == 0 && <p className="Starting-Header-message">Welcome to the ChatBot project! Send a message using the text box below </p>}
