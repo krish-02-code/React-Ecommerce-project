7e0dc15bcc24b309b8c0101320fe2ceee3b4554d
Krish Prajapati <krishprajapati463@gmail.com>
2026-01-05 19:43:09 +0530

    Seprated the checkout pages into seprate folder and updated the checkoutheader into seprate page

--- Ecommerse-project/src/App.jsx
+++ Ecommerse-project/src/App.jsx
@@ -1,6 +1,6 @@
 import { Routes,Route } from 'react-router';
 import { HomePage } from './Pages/HomePage';
-import { CheckoutPage } from './Pages/CheckoutPage';
+import { CheckoutPage } from './Pages/checkout/CheckoutPage';
 import { Orders } from './Pages/Orders';
 import { TrackingPage } from './Pages/TrackingPage';
 import './App.css'
