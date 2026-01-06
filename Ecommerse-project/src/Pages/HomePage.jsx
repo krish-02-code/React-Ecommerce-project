294e758d98134daa73d57c6f69e9de4a76979a8e
Krish Prajapati <krishprajapati463@gmail.com>
2026-01-06 20:53:36 +0530

    Add server proxy config for /api and /images

--- Ecommerse-project/src/Pages/HomePage.jsx
+++ Ecommerse-project/src/Pages/HomePage.jsx
@@ -11,17 +11,17 @@ export function HomePage() {
     // }).then((data)=>
     //     {console.log(data)}
     // );
+
     const [products,SetProducts] = useState([]);
     const [cart,setCart] = useState([]);
 
     useEffect(() => {
-        axios.get('http://localhost:3000/api/products')
+        axios.get('/api/products')
             .then((response) => {
                 SetProducts(response.data);
             });
 
-
-            axios.get('http://localhost:3000/api/cart-items')
+            axios.get('/api/cart-items')
             .then((response)=>{
                 setCart(response.data);
             })
