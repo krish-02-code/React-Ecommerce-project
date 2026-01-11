585eb3099c258460e1acc0536c3c08050f5cb334
Krish Prajapati <krishprajapati463@gmail.com>
2026-01-11 20:33:50 +0530

    Create integration test for product components

--- Ecommerse-project/src/Pages/home/Products.jsx
+++ Ecommerse-project/src/Pages/home/Products.jsx
@@ -33,6 +33,7 @@ export function Product({ product, loadCart }) {
         <div key={product.id} className="product-container">
             <div className="product-image-container">
                 <img className="product-image"
+                data-testid = "product-image"
                     src={product.image} />
             </div>
             <div className="product-name limit-text-to-2-lines">
@@ -41,6 +42,7 @@ export function Product({ product, loadCart }) {
 
             <div className="product-rating-container">
                 <img className="product-rating-stars"
+                     data-testid="rating-image"
                     src={`images/ratings/rating-${product.rating.stars * 10}.png`} />
                 <div className="product-rating-count link-primary">
                     {product.rating.count}
